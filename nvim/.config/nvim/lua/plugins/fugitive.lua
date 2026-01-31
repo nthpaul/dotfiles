e09ca15c25f263517ad6bbf0908ed107f06a936d
Paul Le <nthpaul@proton.me>
2026-01-31 18:57:22 -0500

    2026-01-31 update fugitive keybindings

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -2,6 +2,11 @@ return {
 	"tpopp/vim-fugitive",
 	config = function()
 		vim.keymap.set("n", "<leader>gs", "<cmd>Git<cr>", { desc = "Open git status" })
+		vim.keymap.set("n", "<leader>gv", "<cmd>Gvdiffsplit<cr>", { desc = "Fugitive vertical diff" })
+		vim.keymap.set("n", "<leader>gH", "<cmd>Gdiffsplit<cr>", { desc = "Fugitive horizontal diff" })
+		vim.keymap.set("n", "<leader>g1", "<cmd>diffget //1<cr>", { desc = "Fugitive diffget base" })
+		vim.keymap.set("n", "<leader>g2", "<cmd>diffget //2<cr>", { desc = "Fugitive diffget ours" })
+		vim.keymap.set("n", "<leader>g3", "<cmd>diffget //3<cr>", { desc = "Fugitive diffget theirs" })
 	end,
 	event = "VeryLazy",
 }
